bbe696ebe83cc88a9ca33174285c417e865945a7
Yehor Kochetov <ametionqq@gmail.com>
2023-11-06 16:04:03 +0100

    fix problem with in use dont set after adding build after check route work

--- internal/handlers/checkVersionHandler.go
+++ internal/handlers/checkVersionHandler.go
@@ -12,9 +12,24 @@ import (
 
 func CheckVersionsHandler(context *gfx.Context) {
 	goReq := gorequest.New()
+	db := database.GetDatabase()
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if tx.Error != nil {
+		context.SendJSON(500, response.Response{
+			Message: tx.Error.Error(),
+			Code:    500,
+		})
+		return
+	}
 
 	var dbGames []databaseModels.Game
-	if err := database.GetDatabase().Preload("Builds").Find(&dbGames).Error; err != nil {
+	if err := tx.Preload("Builds").Find(&dbGames).Error; err != nil {
+		tx.Rollback()
 		context.SendJSON(400, response.Response{
 			Message: err.Error(),
 			Code:    400,
@@ -25,8 +40,8 @@ func CheckVersionsHandler(context *gfx.Context) {
 	for i := range dbGames {
 		var game response.SteamGameResponse
 		_, _, errors := goReq.Get(fmt.Sprintf("https://api.steamcmd.net/v1/info/%s", dbGames[i].GameId)).EndStruct(&game)
-
 		if errors != nil || len(errors) > 0 {
+			tx.Rollback()
 			context.SendJSON(400, response.Response{
 				Message: errors[0].Error(),
 				Code:    400,
@@ -45,13 +60,23 @@ func CheckVersionsHandler(context *gfx.Context) {
 		}
 
 		if !buildExists {
+			if err := tx.Model(&databaseModels.Build{}).Where("game_id = ?", dbGames[i].Id).Update("in_use", false).Error; err != nil {
+				tx.Rollback()
+				context.SendJSON(400, response.Response{
+					Message: err.Error(),
+					Code:    400,
+				})
+				return
+			}
+
 			newBuild := databaseModels.Build{
 				GameID:  dbGames[i].Id,
 				BuildId: latestBuildId,
-				InUse:   false,
+				InUse:   true,
 			}
 
-			if err := database.GetDatabase().Create(&newBuild).Error; err != nil {
+			if err := tx.Create(&newBuild).Error; err != nil {
+				tx.Rollback()
 				context.SendJSON(400, response.Response{
 					Message: err.Error(),
 					Code:    400,
@@ -61,6 +86,7 @@ func CheckVersionsHandler(context *gfx.Context) {
 
 			notificationErr := discord.SendNotification("New version available for " + dbGames[i].GameName)
 			if notificationErr != nil {
+				tx.Rollback()
 				context.SendJSON(400, response.Response{
 					Message: notificationErr.Error(),
 					Code:    400,
@@ -68,6 +94,14 @@ func CheckVersionsHandler(context *gfx.Context) {
 				return
 			}
 		}
+	}
+
+	if commitErr := tx.Commit().Error; commitErr != nil {
+		context.SendJSON(500, response.Response{
+			Message: commitErr.Error(),
+			Code:    500,
+		})
+		return
 	}
 
 	context.SendJSON(200, response.Response{
